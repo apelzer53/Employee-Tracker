3e96b2e5f00ecc6ea3bb97bc958db83d9a0d222f
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2024-11-12 13:25:05 -0800

    added code to sort array objects (appointments) by date. Also added 8 additional appointments for testing. The datetime functionality works but additional work is needed to ensure consistent time zones within the data.

--- assets/JS/home.js
+++ assets/JS/home.js
@@ -2,34 +2,117 @@
     from local storage and putting it on the index.html page */
 const apptArray = [];
 
+/*TODO: It may be a good idea to provide a way to filter these dates compared to the current date
+    because appointments in the future may  */
+
+
 function sortByTime(){
-    // console.log("appointments: " + JSON.stringify(apptArray));
-    // console.log("------------------------------------------");
+    /*This arrow function is used to find the difference between the dates
+     The array sort method sorts the array based on the results of the arrow function*/
+    /*https://chatgpt.com/share/67324451-5a8c-8012-8c7e-d26b3c1277e8 */
+    /*https://www.geeksforgeeks.org/arrow-functions-in-javascript/ */
+    apptArray.sort((a,b) => a.dayAndTime.dateTime - b.dayAndTime.dateTime);
+}
+
+//!!! This function is not currently being used
+function isDate(obj){
+    return Object.prototype.toString.call(obj) === "[object Date]";
+}
+
+function outputArray(){
     for(let i = 0; i < apptArray.length; i++){
-        console.log(apptArray[i].dayAndTime.toLocaleString());
+        console.log("appointment " + (i + 1) + ": \n" + "owner:" +  JSON.stringify(apptArray[i].owner) + '\n pet: ' + JSON.stringify(apptArray[i].pet)
+        + " \n service: " + JSON.stringify(apptArray[i].service) + " \n date and time: " + JSON.stringify(apptArray[i].dayAndTime));
+        console.log("------------------------------------------");
+        //string variations
+        //JSON.stringify(apptArray[i].dayAndTime)
+        //apptArray[i].dayAndTime.dateTime.toLocaleString()
     }
 }
 
+//create test data
+//!!! We will need to standardize services if we want to be able to sort by them. This could be done using a select in the form
+    //We would also need a form select that will show the service names that correspond to the service type the user has selected.
+
+//https://www.w3schools.com/js/js_dates.asp
+//these times are not adjusted for time zones
 
 apptArray.push(createAppointment(
     createOwner('John', '456-123-5632'),
     createPet('Sam', 'Great Dane', 2),
-    createService('full wash', 'wash'),
-    createDayAndTime('2024-04-05')
+    createService('bathing and drying', 'basic grooming'),
+    createDayAndTime(new Date())
 ));
 
 apptArray.push(createAppointment(
     createOwner('Jane', '123-455-6354'),
     createPet('Buddy', 'lab', 7),
-    createService('nail trim', 'nails'),
-    createDayAndTime(new Date())
+    createService('nail trim', 'basic grooming'),
+    createDayAndTime('2024-04-05')
 ));
 
+apptArray.push(createAppointment(
+    createOwner('Molly Anderson','555-123-4567'),
+    createPet('Bella', 'Golden Retriever', 3),
+    createService('flea and tick treatment', 'skin and coat'),
+    createDayAndTime('2023-03-08T10:30:00')
+    
+));
 
-for(let i = 0; i < apptArray.length; i++){
-    console.log("appointment " + (i + 1) + ": \n" + "owner:" +  JSON.stringify(apptArray[i].owner) + '\n pet: ' + JSON.stringify(apptArray[i].pet)
-    + " \n service: " + JSON.stringify(apptArray[i].service) + " \n date and time: " + JSON.stringify(apptArray[i].dayAndTime));
-    console.log("------------------------------------------");
-}
+apptArray.push(createAppointment(
+    createOwner('James Campbell','555-234-5678'),
+    createPet('Max', 'Beagle', 5),
+    createService('full body haircut', 'haircuts and styling'),
+    createDayAndTime('2023-07-21')
+));
 
+apptArray.push(createAppointment(
+    createOwner('William Price','555-456-3210'),
+    createPet('Daisy', 'Poodle', 2),
+    createService('brush and de-shedding', 'skin and coat'),
+    createDayAndTime('2024-03-14T09:00:00')
+    
+));
+
+apptArray.push(createAppointment(
+    createOwner('Isabella Ross','555-345-2109'),
+    createPet('Rocky', 'German Shepherd', 4),
+    createService('de-matting', 'haircuts and styling'),
+    createDayAndTime('2024-12-05T13:50:00')
+    
+));
+
+apptArray.push(createAppointment(
+    createOwner('Emma Rivera','555-345-6789'),
+    createPet('Luna', 'Labrador Retriever', 1),
+    createService('flea and tick treatment', 'skin and coat'),
+    createDayAndTime('2024-04-27T11:30:00')
+));
+
+apptArray.push(createAppointment(
+    createOwner('Liam Thompson','555-456-7890'),
+    createPet('Charlie', 'Bulldog', 0.5),
+    createService('nail trim', 'basic grooming'),
+    createDayAndTime('2025-01-10T08:45:00')
+));
+
+apptArray.push(createAppointment(
+    createOwner('Sophia Martinez','555-567-8901'),
+    createPet('Milo', 'Dachshund', 2),
+    createService('bathing and drying', 'basic grooming'),
+    createDayAndTime('2024-04-06')
+));
+
+apptArray.push(createAppointment(
+    createOwner('Noah Stewart','555-789-0123'),
+    createPet('Luna', 'Bulldog', 6),  
+    createService('nail trim', 'basic grooming'),  
+    createDayAndTime('2024-12-05T07:00:00')
+));
+
+//call functions
+console.log("Array before sort: ");
+outputArray()
 sortByTime();
+console.log("Array after sort: ");
+outputArray()
